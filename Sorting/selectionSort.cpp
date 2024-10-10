2358791bfabcda358b692e781f8ed59faa93b560
Zhozemir Kushev <147644881+Zhozemir@users.noreply.github.com>
2024-10-10 15:57:59 +0300

    Update selectionSort.cpp

--- Sorting/selectionSort.cpp
+++ Sorting/selectionSort.cpp
@@ -1,34 +1,34 @@
-//#include <iostream>
-//
-//template <class T>
-//
-//void selectionSort(T* arr, int size) {
-//
-//	for (int i = 0; i < size - 1; i++) {
-//
-//		int minElementIndex = i;
-//
-//		for (int j = i + 1; j < size; j++) {
-//		
-//			if (arr[j] < arr[minElementIndex])
-//				minElementIndex = j;
-//		
-//		}
-//
-//		if (i != minElementIndex)
-//			std::swap(arr[i], arr[minElementIndex]);
-//
-//	}
-//
-//}
-//
-//int main() {
-//
-//	int arr[] = { 4, 1, 3, 9, 8 };
-//
-//	selectionSort(arr, 5);
-//
-//	for (int num : arr)
-//		std::cout << num;
-//
-//}
+#include <iostream>
+
+template <class T>
+
+void selectionSort(T* arr, int size) {
+
+	for (int i = 0; i < size - 1; i++) {
+
+		int minElementIndex = i;
+
+		for (int j = i + 1; j < size; j++) {
+		
+			if (arr[j] < arr[minElementIndex])
+				minElementIndex = j;
+		
+		}
+
+		if (i != minElementIndex)
+			std::swap(arr[i], arr[minElementIndex]);
+
+	}
+
+}
+
+int main() {
+
+	int arr[] = { 4, 1, 3, 9, 8 };
+
+	selectionSort(arr, 5);
+
+	for (int num : arr)
+		std::cout << num;
+
+}
