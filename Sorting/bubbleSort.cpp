d845bbc5b53a0a4866da2d6c58e23f5eed784cd1
Zhozemir Kushev <147644881+Zhozemir@users.noreply.github.com>
2024-10-10 15:58:19 +0300

    Update bubbleSort.cpp

--- Sorting/bubbleSort.cpp
+++ Sorting/bubbleSort.cpp
@@ -1,43 +1,43 @@
-//#include <iostream>
-//
-//template <class T>
-//void bubbleSort(T* arr, unsigned len) {
-//
-//
-//	unsigned end = len - 1;
-//
-//	for (int i = 0; i < len; i++) {
-//	
-//		unsigned lastSwappedIndex = 0;
-//
-//		for (int j = 0; j < end; j++) {
-//		
-//			if (arr[j] > arr[j + 1]) {
-//			
-//				std::swap(arr[j], arr[j + 1]);
-//				lastSwappedIndex = j;
-//			
-//			}
-//		
-//		
-//		}
-//	
-//		end = lastSwappedIndex;
-//		if (lastSwappedIndex == 0)
-//			return;
-//
-//	
-//	}
-//
-//}
-//
-//int main() {
-//
-//	int arr[] = { 5, 1, 3, 9, 4, 3 };
-//
-//	bubbleSort<int>(arr, 6);
-//
-//	for (int num : arr)
-//		std::cout << num;
-//
-//}
+#include <iostream>
+
+template <class T>
+void bubbleSort(T* arr, unsigned len) {
+
+
+	unsigned end = len - 1;
+
+	for (int i = 0; i < len; i++) {
+	
+		unsigned lastSwappedIndex = 0;
+
+		for (int j = 0; j < end; j++) {
+		
+			if (arr[j] > arr[j + 1]) {
+			
+				std::swap(arr[j], arr[j + 1]);
+				lastSwappedIndex = j;
+			
+			}
+		
+		
+		}
+	
+		end = lastSwappedIndex;
+		if (lastSwappedIndex == 0)
+			return;
+
+	
+	}
+
+}
+
+int main() {
+
+	int arr[] = { 5, 1, 3, 9, 4, 3 };
+
+	bubbleSort<int>(arr, 6);
+
+	for (int num : arr)
+		std::cout << num;
+
+}
